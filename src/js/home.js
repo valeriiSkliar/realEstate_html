860785f7c31ab84271c0fd8f66297fa1f7406845
vs <valeriisklyarov@gmail.com>
2025-04-26 22:37:35 +0300

    feat(home/modal): add test modal and trigger button functionality

--- src/js/home.js
+++ src/js/home.js
@@ -1,16 +1,22 @@
-import { showModal, hideModal, showToast, createAndShowToast } from './utils/uiHelpers';
+import {
+  showModal,
+  hideModal,
+  showToast,
+  createAndShowToast,
+} from "./utils/uiHelpers";
 // Home page specific JavaScript
-console.log('Home page loaded'); 
+console.log("Home page loaded");
 
 /**
  * Initialize profile page functionality
  */
 export const initHomePage = () => {
+  createAndShowToast("Test toast for home page!", "success");
 
-    // Test modal
-    // showModal('loginModal');
-
-    // Test toast
-    createAndShowToast('Test toast for home page!', 'success');
-  };
-
+  const testModalButton = document.querySelector(".js-test-modal-trigger");
+  if (testModalButton) {
+    testModalButton.addEventListener("click", () => {
+      showModal("testModal");
+    });
+  }
+};
