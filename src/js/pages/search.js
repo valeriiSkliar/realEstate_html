baf106806998ce4236ef447d596baed3ad337ecf
vs <valeriisklyarov@gmail.com>
2025-06-08 23:12:49 +0300

    feat(search-page): add toast notification button and functionality

--- src/js/pages/search.js
+++ src/js/pages/search.js
@@ -1,6 +1,7 @@
 // src/js/pages/search.js - Скрипты для страницы поиска
 import "../../scss/pages/_search.scss";
 import { initSearchSortButton, initSidebarFilters } from "../components";
+import { createAndShowToast } from "../utils/uiHelpers";
 
 export const initSearchPage = () => {
   const propertyType = document.querySelector("#property_type_select");
@@ -24,6 +25,25 @@ export const initSearchPage = () => {
   }
 
   initSidebarFilters();
+
+  // Инициализация кнопки тестирования toast
+  const testToastBtn = document.querySelector("#testToastBtn");
+  if (testToastBtn) {
+    let toastCounter = 0;
+    const toastTypes = ["success", "error", "warning", "info"];
+    const toastMessages = [
+      "Успешное выполнение операции!",
+      "Произошла ошибка при выполнении",
+      "Внимание! Проверьте введенные данные",
+      "Информационное сообщение",
+    ];
+
+    testToastBtn.addEventListener("click", () => {
+      const typeIndex = toastCounter % toastTypes.length;
+      createAndShowToast(toastMessages[typeIndex], toastTypes[typeIndex], 3000);
+      toastCounter++;
+    });
+  }
 };
 
 initSearchPage();
