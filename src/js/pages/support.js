c3a097b59a16fdb4ad69826bcbaf7a370a5d9704
vs <valeriisklyarov@gmail.com>
2025-07-02 00:08:23 +0300

    refactor(support-page): comment out support form submission handler

--- src/js/pages/support.js
+++ src/js/pages/support.js
@@ -1,5 +1,4 @@
 // src/js/pages/support.js - Скрипты для страницы поддержки
-import { createAndShowToast, showModal } from "../utils/uiHelpers";
 
 /**
  * Initialize support page functionality
@@ -8,7 +7,7 @@ export const initSupportPage = () => {
   const supportForm = document.getElementById("supportForm");
 
   if (supportForm) {
-    supportForm.addEventListener("submit", handleSupportFormSubmit);
+    // supportForm.addEventListener("submit", handleSupportFormSubmit);
   }
 };
 
@@ -16,40 +15,40 @@ export const initSupportPage = () => {
  * Handle support form submission
  * @param {Event} event - The form submit event
  */
-const handleSupportFormSubmit = (event) => {
-  event.preventDefault();
+// const handleSupportFormSubmit = (event) => {
+//   event.preventDefault();
 
-  // Get form values
-  const name = document.getElementById("supportName").value;
-  const email = document.getElementById("supportEmail").value;
-  const subject = document.getElementById("supportSubject").value;
-  const message = document.getElementById("supportMessage").value;
+//   // Get form values
+//   const name = document.getElementById("supportName").value;
+//   const email = document.getElementById("supportEmail").value;
+//   const subject = document.getElementById("supportSubject").value;
+//   const message = document.getElementById("supportMessage").value;
 
-  try {
-    // TODO: send this data to the server
-    console.log("Support form submitted", { name, email, subject, message });
+//   try {
+//     // TODO: send this data to the server
+//     console.log("Support form submitted", { name, email, subject, message });
 
-    // Randomly throw error for testing (50% chance)
-    if (Math.random() < 0.5) {
-      throw new Error("Random server error");
-    }
-  } catch (error) {
-    console.error("Error submitting support form", error);
-    // TODO: show fail request modal
-    if (document.getElementById("supportFailRequestModal")) {
-      // If modal exists, show it
-      showModal("supportFailRequestModal");
-    } else {
-      // Otherwise, show a toast notification
-      createAndShowToast("Произошла ошибка при отправке запроса", "error");
-    }
-  }
+//     // Randomly throw error for testing (50% chance)
+//     if (Math.random() < 0.5) {
+//       throw new Error("Random server error");
+//     }
+//   } catch (error) {
+//     console.error("Error submitting support form", error);
+//     // TODO: show fail request modal
+//     if (document.getElementById("supportFailRequestModal")) {
+//       // If modal exists, show it
+//       showModal("supportFailRequestModal");
+//     } else {
+//       // Otherwise, show a toast notification
+//       createAndShowToast("Произошла ошибка при отправке запроса", "error");
+//     }
+//   }
 
-  // TODO: show success modal
+//   // TODO: show success modal
 
-  // Reset the form
-  supportForm.reset();
-};
+//   // Reset the form
+//   supportForm.reset();
+// };
 
 document.addEventListener("DOMContentLoaded", function () {
   if (document.querySelector(".support-page")) {
